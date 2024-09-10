ccc430277ec49fa4eb08cc8cbc6292dca4c3f45d
Saksham <sakshamloona9@gmail.com>
2024-09-10 15:27:03 +0530

    Commit

--- app/src/main/java/com/transactiononyourtips/AllTransactionActivity.kt
+++ app/src/main/java/com/transactiononyourtips/AllTransactionActivity.kt
@@ -4,17 +4,18 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.transactiononyourtips.databinding.ActivityMainBinding
+import com.transactiononyourtips.databinding.ActivityAllTransactionBinding
 import kotlin.math.abs
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var TransactionsAdapt: TransactionsAdapter
+class AllTransactionActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAllTransactionBinding
     private lateinit var transactions: ArrayList<Transactions>
+    private lateinit var transactionsAdapter: TransactionsAdapter
     private lateinit var linearLayoutManager: LinearLayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+        binding = ActivityAllTransactionBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         transactions = arrayListOf(
@@ -23,23 +24,18 @@ class MainActivity : AppCompatActivity() {
             Transactions("Salary",100000.00,"This Month Salary","09/10/2024"),
             Transactions("Lunch",-600.05,"Lunch","09/10/2024")
         )
-
-        TransactionsAdapt = TransactionsAdapter(transactions, this)
+        transactionsAdapter = TransactionsAdapter(transactions, this)
         linearLayoutManager = LinearLayoutManager(this)
 
-        binding.recyclerview.apply {
-            adapter = TransactionsAdapt
+        binding.recyclerview1.apply {
+            adapter = transactionsAdapter
             layoutManager = linearLayoutManager
         }
-        addTransaction()
-        allTransactions()
+        close()
         updateDashboard()
     }
 
     private fun updateDashboard() {
-        val totalAmt = transactions.map{ it.amt }.sum()
-        val totalInc = transactions.filter{ it.amt >= 0 }.map{ it.amt }.sum()
-        val totalExp = transactions.filter{ it.amt < 0 }.map{ it.amt }.sum()
         val minSp = transactions.filter{it.amt < 0}.map{it.amt}.maxOrNull()?:0.0
         val maxSp = transactions.filter{it.amt < 0}.map{it.amt}.minOrNull()?:0.0
         val avgSp = transactions.filter{it.amt < 0}.map{it.amt}.average()
@@ -47,13 +43,6 @@ class MainActivity : AppCompatActivity() {
         val minIn = transactions.filter{it.amt > 0}.map{it.amt}.minOrNull()?:0.0
         val avgIn = transactions.filter{it.amt > 0}.map{it.amt}.average()
 
-        if(totalAmt >=0)
-            binding.balance.setTextColor(getResources().getColor(R.color.green))
-        else
-            binding.balance.setTextColor(getResources().getColor(R.color.red))
-        binding.balance.text = "₹%.2f".format(totalAmt)
-        binding.income.text = "₹%.2f".format(totalInc)
-        binding.expense.text = "-₹%.2f".format(abs(totalExp))
         binding.MinSpAmt.text = "-₹%.2f".format(abs(minSp))
         binding.MaxSpAmt.text = "-₹%.2f".format(abs(maxSp))
         binding.AvgSpAmt.text = "-₹%.2f".format(abs(avgSp))
@@ -62,22 +51,12 @@ class MainActivity : AppCompatActivity() {
         binding.AvgInAmt.text = "₹%.2f".format(avgIn)
     }
 
-    private fun allTransactions() {
-        val addT = binding.allTransactions
-        addT.setOnClickListener {
-            val intent = Intent(this, AllTransactionActivity::class.java)
-            startActivity(intent)
-        }
-    }
-
-    private fun addTransaction() {
-        val but = binding.addBtn
+    private fun close() {
+        val but = binding.closeBtn
         but.setOnClickListener {
-            val intent = Intent(this, TransactionActivity::class.java)
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
-
-
-
 }
+
