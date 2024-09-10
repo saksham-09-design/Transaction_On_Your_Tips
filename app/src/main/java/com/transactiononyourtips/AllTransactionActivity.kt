0583ff20fb255c35cc560ca777aae57038dfe618
Saksham <sakshamloona9@gmail.com>
2024-09-10 19:02:47 +0530

    Commit

--- app/src/main/java/com/transactiononyourtips/AllTransactionActivity.kt
+++ app/src/main/java/com/transactiononyourtips/AllTransactionActivity.kt
@@ -4,7 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
 import com.transactiononyourtips.databinding.ActivityAllTransactionBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import kotlin.math.abs
 
 class AllTransactionActivity : AppCompatActivity() {
@@ -12,6 +15,7 @@ class AllTransactionActivity : AppCompatActivity() {
     private lateinit var transactions: List<Transactions>
     private lateinit var transactionsAdapter: TransactionsAdapter
     private lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var dataBase: AppDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +23,9 @@ class AllTransactionActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         transactions = arrayListOf()
+
+        dataBase = Room.databaseBuilder(this, AppDatabase::class.java, "transaction_table").build()
+
         transactionsAdapter = TransactionsAdapter(transactions, this)
         linearLayoutManager = LinearLayoutManager(this)
 
@@ -26,8 +33,85 @@ class AllTransactionActivity : AppCompatActivity() {
             adapter = transactionsAdapter
             layoutManager = linearLayoutManager
         }
+
         close()
         updateDashboard()
+        all()
+        pastMonth()
+        pastYear()
+        pastSixMonth()
+    }
+
+    private fun pastMonth(){
+        val pm = binding.pastMonth
+        pm.setOnClickListener {
+            transactions = arrayListOf()
+            GlobalScope.launch {
+                transactions = dataBase.transactionDO().selectPrevData()
+                runOnUiThread {
+                    transactionsAdapter.setData(transactions)
+                    updateDashboard()
+                }
+            }
+        }
+    }
+
+    private fun pastSixMonth(){
+        val psm = binding.sixMonths
+        psm.setOnClickListener {
+            transactions = arrayListOf()
+            GlobalScope.launch {
+                transactions = dataBase.transactionDO().selectsixMonth()
+                runOnUiThread {
+                    transactionsAdapter.setData(transactions)
+                    updateDashboard()
+                }
+            }
+        }
+    }
+
+    private fun all(){
+        val al = binding.allTrans
+        al.setOnClickListener {
+            transactions = arrayListOf()
+            GlobalScope.launch {
+                transactions = dataBase.transactionDO().selectAllData()
+                runOnUiThread {
+                    transactionsAdapter.setData(transactions)
+                    updateDashboard()
+                }
+            }
+        }
+    }
+
+    private fun pastYear(){
+        val py = binding.oneYear
+        py.setOnClickListener {
+            transactions = arrayListOf()
+            GlobalScope.launch {
+                transactions = dataBase.transactionDO().selectPastYear()
+                runOnUiThread {
+                    transactionsAdapter.setData(transactions)
+                    updateDashboard()
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        fetchTransactions()
+    }
+
+    private fun fetchTransactions() {
+        GlobalScope.launch {
+            transactions = dataBase.transactionDO().selectAllData()
+
+            runOnUiThread {
+                transactionsAdapter.setData(transactions)
+                updateDashboard()
+            }
+        }
     }
 
     private fun updateDashboard() {
--- app/src/main/java/com/transactiononyourtips/TransactionDO.kt
+++ app/src/main/java/com/transactiononyourtips/TransactionDO.kt
@@ -7,6 +7,15 @@ interface TransactionDO {
     @Query("SELECT * FROM transaction_table WHERE dateOT >= date('now', 'start of month')")
     fun selectData():List<Transactions>
 
+    @Query("SELECT * FROM transaction_table WHERE dateOT >= date('now', 'start of month', '-1 month') AND dateOT < date('now', 'start of month')")
+    fun selectPrevData():List<Transactions>
+
+    @Query("SELECT * FROM transaction_table WHERE dateOT >= date('now', 'start of month', '-6 month') AND dateOT <= date('now')")
+    fun selectsixMonth():List<Transactions>
+
+    @Query("SELECT * FROM transaction_table WHERE dateOT >= date('now', 'start of month', '-1 year') AND dateOT <= date('now')")
+    fun selectPastYear():List<Transactions>
+
     @Query("SELECT * FROM transaction_table")
     fun selectAllData():List<Transactions>
 
